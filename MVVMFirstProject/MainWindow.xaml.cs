c49e65acc5a3bd70c2671a44f681312499de8554
Nghia Pham Trong <nghiaphamjb@gmail.com>
2024-06-30 10:58:08 +0700

    Add project files.

---
+++ MVVMFirstProject/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using MVVMFirstProject.ViewModel;
+using System.Windows;
+
+namespace MVVMFirstProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainWindowViewModel viewModel = new MainWindowViewModel();
+            DataContext = viewModel;
+        }
+    }
+}
---
+++ MVVMFirstProject/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using MVVMFirstProject.Model;
+using MVVMFirstProject.MVVM;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MVVMFirstProject.ViewModel
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<Item> Items { get; set; }
+
+        public MainWindowViewModel()
+        {
+            Items = new ObservableCollection<Item>();
+            Items.Add(new Item()
+            {
+                Name = "Product1",
+                Description = "Description1",
+                Quantity = 1,
+            });
+            Items.Add(new Item()
+            {
+                Name = "Product2",
+                Description = "Description2",
+                Quantity = 2,
+            });
+            Items.Add(new Item()
+            {
+                Name = "Product3",
+                Description = "Description3",
+                Quantity = 3,
+            });
+
+        }
+
+        private Item selectedItem;
+
+
+        public Item SelectedItem
+        {
+            get { return selectedItem; }
+            set
+            {
+                selectedItem = value;
+                OnPropertyChanged();
+            }
+        }
+
+       
+
+    }
+}
